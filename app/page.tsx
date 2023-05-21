cee09871fcb12446951030c807c555fc2b1f6237
takeuchi-masaki <themasakitakeuchi@gmail.com>
2023-05-21 10:09:29 -0700

    update favicon, logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}> Welcome to DOITTER</h1>
+      <Image src="/logo.jpeg" alt={''} width={100} height={100}></Image>
+      <h1 className={styles.title}> Welcome to DoItter</h1>
       <Link href="/api/auth/login" className={styles.link}>
         Begin Sharing
       </Link>
