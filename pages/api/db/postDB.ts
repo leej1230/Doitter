c169d0cf91ec33c4f0826c00528f6fb0d7e98ffa
leej1230 <leejaewoo1230@gmail.com>
2023-05-20 20:45:49 -0700

    post api is done

--- pages/api/db/postDB.ts
+++ pages/api/db/postDB.ts
@@ -28,10 +28,23 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     // RESPONSE POST REQUESTS -> POST new post 
     POST: async (req: NextApiRequest, res: NextApiResponse) => {
         try {
-            const postData = JSON.parse(req.body); // parse the request body
-            const createdPost = await Post.create(postData); // create a new todo
+            console.log(req.query);
+
+            const parsedQuery = Object.fromEntries(
+                Object.entries(req.query).map(([key, value]) => {
+                    if (Array.isArray(value)) {
+                        return [key, value.map(item => JSON.parse(item))];
+                    } else if (typeof value === 'string') {
+                        return [key, JSON.parse(value)];
+                    }
+                    return [key, value];
+                })
+            );
+
+            const createdPost = await Post.create(parsedQuery); // create a new todo
             res.json(createdPost); // send the created todo as the response
         } catch(e) {
+            console.log(e);
             catcher(e);
         }
     },
@@ -39,14 +52,15 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     PUT:async (rep:NextApiRequest, res: NextApiResponse) => {
         
         // Check if the request json has field liked_user
-        if (rep.body.liked_user) {
+        if (rep.query.liked_user) {
             // Request for updating Liked_users of the post
             try {
-                await Post.findByIdAndUpdate(
-                  rep.body._id,
-                  { $push: { liked_users: rep.body.liked_user } },
+                const updateLikedList = await Post.findByIdAndUpdate(
+                  rep.query._id,
+                  { $push: { liked_users: rep.query.liked_user } },
                   { new: true }
                 );
+                res.json(updateLikedList)
             } catch (e) {
                 catcher(e)
             }
@@ -55,11 +69,12 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
 
             // Request for updating check mark
             try {
-                await Post.findOneAndUpdate(
-                    {_id:rep.body._id},
-                    {$set: {[`checked.${rep.body.index}`]: rep.body.newValue}},
+                const updateChecked = await Post.findOneAndUpdate(
+                    {_id:rep.query._id},
+                    {$set: {[`checked.${rep.query.index}`]: rep.query.newValue}},
                     {new: true}
-                )
+                    )
+                res.json(updateChecked);
             } catch (e) {
                 catcher(e)
             }
@@ -75,3 +90,4 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
   if (response) response(req, res)
   else res.status(400).json({ error: "No Response for This Request" })
 }
+
